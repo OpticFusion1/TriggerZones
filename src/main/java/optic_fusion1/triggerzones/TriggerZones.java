e7002be0819adde4a1451d9e9b6551b794b055c0
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2026-02-24 17:11:20 +0100

    Initial Commit

---
+++ src/main/java/optic_fusion1/triggerzones/TriggerZones.java
@@ -0,0 +1,11 @@
+package optic_fusion1.triggerzones;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TriggerZones extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
